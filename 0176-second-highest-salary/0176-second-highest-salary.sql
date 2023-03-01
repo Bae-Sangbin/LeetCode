ef43330ca6e9d82dab7f40034364a9c18b528b39
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-01 19:31:56 +0900

    Time: 414 ms (51.98%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -9,3 +9,6 @@ SELECT MAX(SALARY) AS 'SecondHighestSalary'
 FROM (SELECT SALARY, DENSE_RANK() OVER(ORDER BY SALARY DESC) AS RAN
      FROM Employee) AS TBL
 WHERE RAN = 2;
+
+# 꼭 DENSE_RANK로 해야 합니다.
+# DENSE_RANK() -> RANK(), ROW_NUMBER()로 쓰면 반례가 생겨서 오답입니다.
