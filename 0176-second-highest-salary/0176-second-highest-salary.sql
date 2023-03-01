e1aea5b36c37b4fd5559412be980ec0e48ae5288
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-01 19:33:08 +0900

    Time: 528 ms (15.33%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -5,15 +5,10 @@
 # WHERE SALARY NOT IN (SELECT MAX(SALARY)
 #                     FROM Employee)
                     
-# SELECT MAX(SALARY) AS 'SecondHighestSalary'
-# FROM (SELECT SALARY, DENSE_RANK() OVER(ORDER BY SALARY DESC) AS RAN
-#      FROM Employee) AS TBL
-# WHERE RAN = 2;
+SELECT MAX(SALARY) AS 'SecondHighestSalary'
+FROM (SELECT SALARY, DENSE_RANK() OVER(ORDER BY SALARY DESC) AS RAN
+     FROM Employee) AS TBL
+WHERE RAN = 2;
 
 # 꼭 DENSE_RANK로 해야 합니다.
 # DENSE_RANK() -> RANK(), ROW_NUMBER()로 쓰면 반례가 생겨서 오답입니다.
-
-SELECT IFNULL((SELECT DISTINCT salary 
-                FROM Employee 
-                ORDER BY salary DESC 
-                LIMIT 1,1 ),null) AS SecondHighestSalary
