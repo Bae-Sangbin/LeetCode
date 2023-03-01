ae5a5acc13680c328ba2345b6db7feb764c6bcfe
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-01 17:40:38 +0900

    Time: 438 ms (41.16%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
 
+# SELECT MAX(SALARY) AS 'SecondHighestSalary'
+# FROM Employee
+# WHERE SALARY NOT IN (SELECT MAX(SALARY)
+#                     FROM Employee)
+                    
 SELECT MAX(SALARY) AS 'SecondHighestSalary'
-FROM Employee
-WHERE SALARY NOT IN (SELECT MAX(SALARY)
-                    FROM Employee)
+FROM (SELECT SALARY, DENSE_RANK() OVER(ORDER BY SALARY DESC) AS R
+     FROM Employee) AS SALARY_RANK
+WHERE R = 2;
