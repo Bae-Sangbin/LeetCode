f950d5f6404a325328efb48611a81a8c8bb54d95
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-02 22:23:08 +0900

    Time: 903 ms (12.48%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -3,10 +3,11 @@ BEGIN
   RETURN (
       SELECT DISTINCT salary 
       FROM ( 
-            SELECT salary,
-            DENSE_RANK() OVER (ORDER BY salary DESC) as RAN
-          FROM employee
-          ) TBL
+            SELECT 
+                salary,
+                DENSE_RANK() OVER (ORDER BY salary DESC) as RAN
+            FROM employee
+           ) TBL
       WHERE RAN=N
-  );
+);
 END
