2cf36bc72679be99a010b282913b71aef20bb32f
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-05 09:55:32 +0900

    Time: 1497 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -15,7 +15,5 @@
 # AND DATEDIFF(A.recordDate,B.PREV_DATE) = 1 
 
 select w1.id
-from Weather w1, Weather w2
-where w1.Temperature > w2.Temperature
-and datediff(w1.recordDate, w2.recordDate) = 1
-;
+from Weather w1 JOIN Weather w2 ON w1.Temperature > w2.Temperature
+and datediff(w1.recordDate, w2.recordDate) = 1;
