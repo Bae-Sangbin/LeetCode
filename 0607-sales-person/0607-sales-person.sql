5da830b772804e3838ca98fcc4dce16dbdd44d1d
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-14 22:50:26 +0900

    Time: 3668 ms (5.03%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -7,9 +7,10 @@
 
 # 이름이 "RED"인 회사와 관련된 주문이 없는 모든 영업 사원의 이름을 보고하는 SQL 쿼리를 작성하십시오.
 
-select s.name 
-from SalesPerson s
-where s.sales_id not in ( (select o.sales_id
-    from orders o
-    inner join company c on o.com_id = c.com_id
-    where c.name = 'RED'))
+SELECT s.name
+FROM SalesPerson s
+WHERE s.name NOT IN (
+SELECT s.name
+FROM orders o JOIN company c ON o.com_id = c.com_id  
+JOIN SalesPerson s ON o.sales_id = s.sales_id
+WHERE c.name ='RED')
