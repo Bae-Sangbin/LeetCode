e612473cbe6a01d9443709545fd01dd21bda8db5
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-14 22:31:42 +0900

    Time: 1132 ms (80.64%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -5,8 +5,9 @@
 
 # 각 부서에서 급여가 가장 높은 직원을 찾는 SQL 쿼리를 작성하세요.
 
-SELECT Department.name AS 'Department', Employee.name AS 'Employee', Salary
-FROM Employee JOIN Department ON Employee.DepartmentId = Department.Id 
-WHERE(Employee.DepartmentId , Salary)IN(SELECT DepartmentId, MAX(Salary)
-                                        FROM Employee
-                                        GROUP BY DepartmentId);
+SELECT Department, Employee, Salary
+FROM
+(SELECT D.name AS Department, E.name AS Employee, Salary , RANK() OVER(PARTITION BY D.name
+                                                            ORDER BY Salary DESC) AS RAN
+FROM Employee E JOIN Department D ON E.departmentId = D.id) TBL
+WHERE RAN = 1
