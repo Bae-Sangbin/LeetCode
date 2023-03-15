3831585a2c3ae7f9e427cdfacf61a277396ca86a
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-15 22:43:30 +0900

    Time: 2333 ms (15.05%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -2,4 +2,4 @@
 SELECT S.product_id, product_name
 FROM Sales S JOIN Product P ON S.product_id = P.product_id
 GROUP BY S.product_id, P.product_name
-HAVING MIN(S.sale_date) >= '2019-01-01' AND max(S.sale_date) <= '2019-03-31';
+HAVING MIN(S.sale_date) >= '2019-01-01' AND MAX(S.sale_date) <= '2019-03-31';
