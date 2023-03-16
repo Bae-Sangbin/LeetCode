d4d33a299d0e38c16d315fd4826a3e047001a901
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-16 16:30:10 +0900

    Time: 999 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 SELECT id, 
 CASE 
-WHEN (id % 2 = 0) then lag(student, 1) over(ORDER BY id)
+WHEN (id % 2 = 0) THEN lag(student) OVER(ORDER BY id)
 WHEN id = (SELECT MAX(id) FROM Seat) then student
-WHEN(id % 2 > 0) THEN lead(student, 1) OVER(ORDER BY id)
+WHEN(id % 2 > 0) THEN lead(student) OVER(ORDER BY id)
 END student
 FROM Seat
