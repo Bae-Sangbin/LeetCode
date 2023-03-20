86df08776d3f223ffcb50caebb61186d5ecb1344
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-20 11:36:29 +0900

    Time: 667 ms (24.03%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,7 +1,7 @@
 select  
     case 
     when id % 2 = 0 then id - 1
-    when id % 2 = 1 and id < (select count(*) from seat) then id + 1  # 서브쿼리 대신 max(id) 사용이 불가능한가?
+    when id % 2 = 1 and id < (select max(id) from seat) then id + 1  # 서브쿼리 대신 max(id) 사용이 불가능한가?
     else id
     end as id, student 
 from seat
