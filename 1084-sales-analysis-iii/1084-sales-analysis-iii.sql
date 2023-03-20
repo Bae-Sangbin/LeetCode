0c60da759a4bd565c6af1b87600e50a9cbc1cb25
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-20 18:11:18 +0900

    Time: 2466 ms (8.41%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,5 +1,4 @@
-# 왜 틀린건지 모르겠습니다
-select p.product_id, p.product_name
-from Product p join Sales s on p.product_id = s.product_id
-group by p.product_id
-having MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31'
+SELECT S.product_id, product_name
+FROM Sales S JOIN Product P ON S.product_id = P.product_id
+GROUP BY S.product_id
+HAVING MIN(S.sale_date) >= '2019-01-01' AND MAX(S.sale_date) <= '2019-03-31';
