16bd68cd2dce9d2626ebc920425d84632012dab7
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-22 18:33:20 +0900

    Time: 2304 ms (60.10%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -3,5 +3,5 @@ SELECT user_id AS buyer_id, join_date,
 COUNT(order_id) AS orders_in_2019
 FROM Users LEFT JOIN (SELECT order_id, buyer_id
                       FROM Orders
-                      WHERE order_date LIKE '2019%') TBL ON user_id = buyer_id 
+                      WHERE YEAR(order_date) = 2019) TBL ON user_id = buyer_id 
 GROUP BY user_id;
