5201fddebc4cd8b7e784dff94dcae27f6e6a34bc
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-22 14:39:01 +0900

    Time: 1488 ms (78.09%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-SELECT u.name , IFNULL(SUM(r.distance), 0) AS travelled_distance  
-From Users u  LEFT JOIN RIDES r
-ON u.id = r.user_id
-GROUP BY u.id
-ORDER BY travelled_distance DESC, u.name;
+SELECT U.name , IFNULL(SUM(r.distance), 0) AS travelled_distance  
+From Users U LEFT JOIN Rides R
+ON U.id = R.user_id
+GROUP BY U.id
+ORDER BY travelled_distance DESC, U.name;
