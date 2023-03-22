b47365c0091e1b570132f88c5d78fcfc125b1be5
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-22 18:31:13 +0900

    Time: 2282 ms (63.59%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT user_id AS buyer_id, join_date,
+COUNT(order_id) AS orders_in_2019
+FROM Users LEFT JOIN (SELECT order_id, buyer_id
+                      FROM Orders
+                      WHERE order_date LIKE '2019%') TBL ON user_id = buyer_id 
+GROUP BY user_id;
