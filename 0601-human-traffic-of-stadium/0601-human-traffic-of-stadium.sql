4839f0f58f0d96f0debaa866af073f8e91e487eb
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-24 17:51:47 +0900

    Time: 786 ms (21.47%), Space: 0B (100.00%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,15 +1,15 @@
-select distinct id, visit_date, people
+select id, visit_date, people
 from (
     select
-        id, visit_date, people,
-        LEAD(people, 1) OVER(ORDER BY id) as lead1,
-        LEAD(people, 2) OVER(ORDER BY id) as lead2,
-        LAG(people, 1) OVER(ORDER BY id) as lag1,
-        LAG(people, 2) OVER(ORDER BY id) as lag2
+        *,
+        LEAD(people, 1) OVER(ORDER BY id) as NEXT_PEOPLE,
+        LEAD(people, 2) OVER(ORDER BY id) as NEXT_PEOPLE2,
+        LAG(people, 1) OVER(ORDER BY id) as PREV_PEOPLE,
+        LAG(people, 2) OVER(ORDER BY id) as PREV_PEOPLE2
     FROM stadium
 ) TBL1
 where people >= 100 AND (
-    (lead1 >= 100 AND lead2 >= 100) OR 
-    (lag1 >= 100 AND lag2 >= 100) OR 
-    (lead1 >= 100 AND lag1 >= 100)
+    (NEXT_PEOPLE >= 100 AND NEXT_PEOPLE2 >= 100) OR 
+    (PREV_PEOPLE >= 100 AND PREV_PEOPLE2 >= 100) OR 
+    (NEXT_PEOPLE >= 100 AND PREV_PEOPLE >= 100)
 )
