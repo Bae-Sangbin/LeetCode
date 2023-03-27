40d332a07843d91f60a1a080c6823e9c43a85e4f
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-27 19:22:00 +0900

    Time: 1493 ms (86.68%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,7 +1,6 @@
-# Write your MySQL query statement below
 SELECT D.name AS Department, E.name AS Employee, Salary
 FROM (SELECT *,
       DENSE_RANK() OVER(PARTITION BY DEPARTMENTID
                  ORDER BY SALARY DESC) AS RAN
      FROM EMPLOYEE) E JOIN DEPARTMENT D ON E.DEPARTMENTID = D.ID
-WHERE RAN IN (1,2,3)
+WHERE RAN <= 3
