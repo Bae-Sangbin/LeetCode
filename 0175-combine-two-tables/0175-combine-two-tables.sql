680c85b71e99dbea2a830f80c973032f585acb7b
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-27 22:31:22 +0900

    Time: 889 ms (22.77%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
 
-SELECT firstName, lastName, city, state
-FROM PERSON A LEFT JOIN ADDRESS B ON A.personId = B.personId
+# Person 테이블에 있는 각 개인의 이름, 성, 도시 및 주를 보고하는 SQL 쿼리를 작성하십시오. personId의 주소가 Address 테이블에 없으면 대신 null을 보고합니다
+
+SELECT FIRSTNAME, LASTNAME, CITY, STATE
+FROM PERSON P LEFT JOIN ADDRESS A ON P.PERSONID = A.PERSONID
