d1e46c393369bfcfb19e1863837659501d25f6a4
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-27 19:22:46 +0900

    Time: 1933 ms (34.60%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,6 +1,18 @@
-SELECT D.name AS Department, E.name AS Employee, Salary
-FROM (SELECT *,
-      DENSE_RANK() OVER(PARTITION BY DEPARTMENTID
-                 ORDER BY SALARY DESC) AS RAN
-     FROM EMPLOYEE) E JOIN DEPARTMENT D ON E.DEPARTMENTID = D.ID
-WHERE RAN <= 3
+WITH rank_dp AS(
+    select
+        id as employee_id,
+        name as employee_name,
+        salary,
+        departmentId,
+        DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary desc) AS rank_salary
+from Employee
+    )
+select 
+    t2.name as Department,
+    t1.employee_name as Employee,
+    t1.salary as Salary
+from rank_dp t1
+left join Department t2 on t1.departmentId = t2.id
+where rank_salary between 1 and 3
+
+# rank_salary <= 3 으로 하면 결과가 잘 안나옴 (왜지ㅠㅠ?)
