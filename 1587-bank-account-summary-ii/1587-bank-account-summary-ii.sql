cced53f0a01b7443df743bd90019896fff01c3bd
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-29 17:05:35 +0900

    Time: 1673 ms (26.27%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -2,8 +2,8 @@
 # 잔액이 10000 이상인 사용자의 이름과 잔액을 보고하는 SQL 쿼리를 작성하세요. 
 # 계정 잔액은 해당 계정과 관련된 모든 거래 금액의 합계와 같습니다
 
-SELECT NAME, T.AMOUNT AS BALANCE
-FROM Users U JOIN (SELECT ACCOUNT, SUM(AMOUNT) AS AMOUNT
+SELECT NAME, BALANCE
+FROM Users U JOIN (SELECT ACCOUNT, SUM(AMOUNT) AS BALANCE
                    FROM Transactions
                    GROUP BY ACCOUNT
                    HAVING SUM(AMOUNT) > 10000) T ON U.ACCOUNT = T.ACCOUNT
