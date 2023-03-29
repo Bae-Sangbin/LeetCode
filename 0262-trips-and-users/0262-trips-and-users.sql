b2ddf719081524cabb73e0489a50e4e395856aab
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-29 14:49:08 +0900

    Time: 1066 ms (57.64%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,10 +1,14 @@
 # Write your MySQL query statement below
 SELECT t.Request_at AS Day,
-        ROUND(SUM(t.Status IN ('cancelled_by_client', 'cancelled_by_driver'))/COUNT(*), 2) AS "Cancellation Rate"
-    FROM Trips t
-    JOIN Users uc ON uc.Users_Id = t.Client_Id
-    JOIN Users ud ON ud.Users_Id = t.Driver_Id 
+ROUND(SUM(t.Status IN ('cancelled_by_client', 'cancelled_by_driver'))/COUNT(*), 2) 
+AS "Cancellation Rate"
+    FROM Trips t JOIN (SELECT *
+                      FROM Users
+                      WHERE Role = 'client') uc ON uc.Users_Id = t.Client_Id
+                 JOIN (SELECT *
+                      FROM Users
+                      WHERE Role = 'driver') ud ON ud.Users_Id = t.Driver_Id 
 WHERE t.Request_at BETWEEN "2013-10-01" AND "2013-10-03"
-AND uc.Role = 'client' AND uc.Banned = "No"
-AND ud.Role = 'driver' AND ud.Banned = "No"
+AND uc.Banned = "No"
+AND ud.Banned = "No"
 GROUP BY Day
