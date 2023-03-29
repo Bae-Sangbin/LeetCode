1171e19f7b5066436c463beef0d3a9848b5fa2c5
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-29 15:06:51 +0900

    Time: 1205 ms (26.20%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -7,9 +7,10 @@ AS "Cancellation Rate"
          WHERE Request_at BETWEEN "2013-10-01" AND "2013-10-03") t 
          JOIN (SELECT *
                FROM Users
-               WHERE Role = 'client') uc ON uc.Users_Id = t.Client_Id
+               WHERE Role = 'client') C ON C.Users_Id = t.Client_Id
          JOIN (SELECT *
                FROM Users
-               WHERE Role = 'driver') ud ON ud.Users_Id = t.Driver_Id 
-WHERE uc.Banned = "No" AND ud.Banned = "No"
+               WHERE Role = 'driver') D ON D.Users_Id = t.Driver_Id 
+WHERE C.Banned = "No" AND D.Banned = "No"
 GROUP BY Day
+ORDER BY Day
