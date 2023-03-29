2c0a966249eac2d915bd109fa5ec8523f65623a8
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-29 14:22:54 +0900

    Time: 1526 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -2,7 +2,9 @@
 SELECT t.Request_at AS Day,
         ROUND(SUM(t.Status IN ('cancelled_by_client', 'cancelled_by_driver'))/COUNT(*), 2) AS "Cancellation Rate"
     FROM Trips t
-    JOIN Users uc ON uc.Users_Id = t.Client_Id AND uc.Role = 'client' AND uc.Banned = "No"
-    JOIN Users ud ON ud.Users_Id = t.Driver_Id AND ud.Role = 'driver' AND ud.Banned = "No"
+    JOIN Users uc ON uc.Users_Id = t.Client_Id
+    JOIN Users ud ON ud.Users_Id = t.Driver_Id 
 WHERE t.Request_at BETWEEN "2013-10-01" AND "2013-10-03"
+AND uc.Role = 'client' AND uc.Banned = "No"
+AND ud.Role = 'driver' AND ud.Banned = "No"
 GROUP BY Day
