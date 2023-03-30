5877cb0933bb36a8d0e62ef54cfa72a10fd47639
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-30 10:23:12 +0900

    Time: 589 ms (98.60%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -4,18 +4,13 @@
 
 # 이전 날짜(어제)에 비해 온도가 높은 모든 날짜의 Id를 찾는 SQL 쿼리를 작성하십시오.
 
-# WITH PREV_TEMP AS
-# (SELECT *, LAG(recordDate,1) OVER(ORDER BY recordDate) AS PREV_DATE,
-#  LAG(temperature,1) OVER(ORDER BY recordDate) AS PREV_TEMPERATURE
-#  FROM Weather)
+WITH PREV_TEMP AS
+(SELECT *, LAG(recordDate,1) OVER(ORDER BY recordDate) AS PREV_DATE,
+ LAG(temperature,1) OVER(ORDER BY recordDate) AS PREV_TEMPERATURE
+ FROM Weather)
 
-# SELECT ID
-# FROM PREV_TEMP
-# WHERE TEMPERATURE > PREV_TEMPERATURE 
-# AND DATEDIFF(recordDate,PREV_DATE) = 1 
+SELECT ID
+FROM PREV_TEMP
+WHERE TEMPERATURE > PREV_TEMPERATURE 
+AND DATEDIFF(recordDate,PREV_DATE) = 1 
 
-
-#버전 1
-SELECT w1.id
-FROM Weather w1 JOIN Weather w2 ON w1.Temperature > w2.Temperature
-AND DATEDIFF(w1.recordDate, w2.recordDate) = 1;
