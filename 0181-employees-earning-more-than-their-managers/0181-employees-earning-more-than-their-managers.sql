314ea72b9bac8409007f00c342016eb27f511c6b
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-03-30 23:35:01 +0900

    Time: 637 ms (88.10%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-# Primary key : ID
-# Foreign Key : ManagerID
+# 관리자보다 더 많은 수입을 올리는 직원을 찾아주세요.
 
-# 셀프 조인
 SELECT E1.NAME AS Employee
 FROM EMPLOYEE E1 JOIN EMPLOYEE E2 ON E1.MANAGERID = E2.ID
 WHERE E1.SALARY > E2.SALARY
